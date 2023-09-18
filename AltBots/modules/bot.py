ad3458f4855ec2400a9e56a6bd30b2ef2fb3687f
ASHWANIashwani <134682768+ASHWANIashwani@users.noreply.github.com>
2023-09-18 13:41:30 +0530

    Update bot.py

--- AltBots/modules/bot.py
+++ AltBots/modules/bot.py
@@ -24,7 +24,7 @@ async def ping(e):
         altron = await e.reply(f"» __ASHWANI__")
         end = datetime.now()
         mp = (end - start).microseconds / 1000
-        await altron.edit(f"__KISKI CHUDAI KARNI HAI__\n» `{mp} ᴍꜱ`")
+        await altron.edit(f"__⚡️𝐀𝐒𝐇𝐖𝐀𝐍𝐈⚡__\n» `{mp} ᴍꜱ`")
 
 
 @X1.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
